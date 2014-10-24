517f256f0593dfc41ec5437a1370bb5ae19854fb
Emerson Lackey <emerson.lackey@gmail.com>
2014-10-24 12:06:56 -0400

    Allowing port argument for Auth

--- Auth/app.js
+++ Auth/app.js
@@ -1,13 +1,24 @@
 #!/usr/bin/env node
 
 var bodyParser = require('body-parser')
+var program = require('commander')
 var express = require('express')
 var app = express()
+
+program
+  .version('0.0.1')
+  .option('-p, --port [port]', 'Specify the port this app should listen on')
+  .parse(process.argv)
+
+var port = 3030;
+if(program.port) {
+  port = program.port
+}
 
 require('./lib/routes')(app)
 app.use(bodyParser.json())
 
-var server = app.listen(3030, function() {
+var server = app.listen(port, function() {
   var host = server.address().address
   var port = server.address().port
   console.log('Example app listening at http://%s:%s', host, port)
